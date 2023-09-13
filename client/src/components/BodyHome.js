038f108fb6b1717e26ef03ebb3e8bba230d610ea
thugperfect <meenamani9444@gmail.com>
2023-09-13 18:35:48 +0530

    styling

--- client/src/components/BodyHome.js
+++ client/src/components/BodyHome.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const BodyHome = () => {
   return (
-    <div className='w-full h-full bg-white '>
-      
+    <div className='w-full flex'>
+      <div className=' w-1/3 min-h-[89vh]  border-r-1 border-slate-700'></div>
+      <div className='w-2/3 min-h-[89vh] ' ></div>
     </div>
   )
 }
--- client/src/components/HeaderHome.js
+++ client/src/components/HeaderHome.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const HeaderHome = () => {
   return (
-    <div className='bg-white dark:bg-black  w-full h-[80px] flex items-center'>
-      <div className='text-gray-600 text-4xl w-[200px] flex justify-center'>El Jury</div>
+    <div className='bg-white dark:bg-black  w-full md:h-[80px] h-[50px]  flex items-center'>
+      <div className='text-gray-600 dark:text-gray-300 text-2xl md:text-4xl w-[150px] md:w-[200px] flex justify-center '>El Jury</div>
     </div>
   )
 }
