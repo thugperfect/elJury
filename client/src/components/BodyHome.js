72d01b519955fe1bcb3f8f6e4efb12d9f20fd00b
thugperfect <meenamani9444@gmail.com>
2023-09-14 14:09:07 +0530

    styling homepage

--- client/src/components/BodyHome.js
+++ client/src/components/BodyHome.js
@@ -8,23 +8,28 @@ const BodyHome = ({ size }) => {
       {size.width > 900 ? (
         <>
           <Login />
-          <div className="w-2/3 min-h-[700px] bg-slate-200 dark:bg-zinc-900 dark:text-white relative bg-backgroung-image bg-cover">
+          <div className="w-2/3 min-h-[700px] bg-slate-200 dark:bg-zinc-900 dark:text-white relative bg-backgroung-image bg-cover flex justify-center items-center">
           
-            <div className="absolute">
-            <div className="">Browse Books</div>
-            <Link to='/signup'><div className="">Join Us</div></Link>
+            <div className="md:flex">
+            <Link to="/book"><div className=" w-[150px] flex justify-center items-center h-[40px] bg-blue-500 cursor-pointer mr-5 mb-5">Browse Books</div></Link>
+            <Link to='/signup'><div className="pr-5 pl-5 pt-2 pb-2 bg-blue-500 cursor-pointer">Join Us</div></Link>
             </div>
           
           </div>
         </>
       ) : (
-        <div className="w-full min-h-[89vh] bg-slate-200 dark:bg-zinc-900 dark:text-white">
-          <div className="">Browse Books</div>
-          <Link to='/signup'><div className="">Join Us</div></Link>
-          <Link to="/login">
-            <div className="">Login</div>
-          </Link>
+        // 
+        <div className="w-full min-h-[700px] bg-slate-200 dark:bg-zinc-900 dark:text-white relative bg-backgroung-image bg-cover flex justify-center items-center">
+          
+        <div className="md:flex">
+        <Link to="/book"><div className=" w-[150px] flex justify-center items-center h-[40px] bg-blue-500 cursor-pointer mr-5 mb-5">Browse Books</div></Link>
+        <Link to='/signup'><div className="w-[150px] flex justify-center items-center h-[40px] bg-blue-500 cursor-pointer mr-5 mb-5">Join Us</div></Link>
+       <Link to='/login'>
+        <div className="w-[150px] flex justify-center items-center h-[40px] bg-blue-500 cursor-pointer">Login</div>
+        </Link> 
         </div>
+      
+      </div>
       )}
     </div>
   );
