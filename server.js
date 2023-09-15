8de08f32f7cb73f74f1fa472f66e7c54e5a9267e
thugperfect <meenamani9444@gmail.com>
2023-09-15 06:56:39 +0530

    mongo connected

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 require('dotenv').config()
 
-const express = require('express')
 
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
 const app = express()
 
+app.use(express.json())
+app.use(cors())
+
 const port = process.env.PORT
-const mongo_url = process.env.MONGO_URL
+const db = process.env.MONGO_URL
+
+mongoose.connect(db,{
+    useUnifiedTopology:true,
+    useNewUrlParser:true
+})
+
+mongoose.connection.on('error',(err)=>{
+    console.log("MongoDB ==> Disconnected");
+    console.log(err);
+})
+mongoose.connection.once('open',()=>{
+    console.log('MongoDB ==> Connected')
+})
 
 app.listen(port,()=>{
     console.log(`SERVER@${port}`);
