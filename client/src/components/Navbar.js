95f09b86bb56760b75bb50039ed8f0dead4dc9a9
thugperfect <meenamani9444@gmail.com>
2023-09-16 13:16:43 +0530

    navbar responsive

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,9 +5,25 @@ import { BiSolidUserCircle,BiMailSend } from "react-icons/bi";
 import { FcAbout } from "react-icons/fc";
 import {useNavigate} from "react-router-dom"
 import logo from '../images/eljurylogo.jpg'
-const Navbar = () => {
+import {useState} from 'react'
+const Navbar = ({size}) => {
   const searchRef = useRef()
   const navigate =  useNavigate()
+
+  const [toggleSearchBar,setToggleSearchBar] = useState(false)
+  const toggleSearch = ()=>{
+    setToggleSearchBar(!toggleSearchBar)
+  }
+
+  const searchBar =   <div className="flex rounded-lg">
+  <div onClick={()=>searchRef.current.focus()} className="w-[30px] out-r  rounded-l-full bg-zinc-950 outline outline-1 outline-slate-500 flex justify-center items-center" ><AiOutlineSearch/></div>
+  <input type="text" ref={searchRef} name="" id="" className="h-[30px] w-[150px] md:w-[200px] rounded-r-full bg-zinc-950 outline outline-1 outline-slate-500 px-2" />
+</div>
+  const searchIcon =<div className='flex flex-col items-center'>
+  <div onClick={toggleSearch} className='w-[40px] h-[40px] rounded-full outline outline-1 outline-gray-400  flex items-center justify-center'><AiOutlineSearch/></div>
+<div className={toggleSearchBar ? "absolute w-[250px] p-5 bg-gray-200 mt-11 rounded-lg flex justify-center ":"hidden"}  >{searchBar}</div>
+  </div>
+
   return (
     <div className="w-full h-[80px] bg-zinc-800  text-white flex justify-between items-center px-2 md:px-5">
       <div className="text-2xl md:text-4xl flex items-center gap-3">
@@ -18,10 +34,9 @@ const Navbar = () => {
       </div>
       <div></div>
       <div></div>
-      <div className="flex rounded-lg">
-        <div onClick={()=>searchRef.current.focus()} className="w-[30px] out-r  rounded-l-full bg-zinc-950 outline outline-1 outline-slate-500 flex justify-center items-center" ><AiOutlineSearch/></div>
-        <input type="text" ref={searchRef} name="" id="" className="h-[30px] w-[150px] md:w-[200px] rounded-r-full bg-zinc-950 outline outline-1 outline-slate-500 px-2" />
-      </div>
+      {
+          size.width>600 ? ( searchBar ):( searchIcon )
+        }
       <div className="flex gap-2 items-center">
         <BiSolidUserCircle className="cursor-pointer w-[20px] h-[20px] md:w-[30px] md:h-[30px]" />
         <BsThreeDotsVertical className="cursor-pointer w-[20px] h-[20px] md:w-[30px] md:h-[30px] rounded-full outline outline-1 outline-slate-400 p-2" />
--- client/src/pages/Index.js
+++ client/src/pages/Index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import { Outlet } from 'react-router-dom'
 
-const Index = () => {
+const Index = ({size}) => {
   return (
-    <div><Navbar/>
+    <div><Navbar size={size}/>
     <Outlet/>
     </div>
   )
