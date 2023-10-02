37e27c4e9c9be149954144af450b7222ad944535
thugperfect <meenamani9444@gmail.com>
2023-10-02 11:58:04 +0530

    controller compete

--- controller/dataController.js
+++ controller/dataController.js
@@ -1,5 +1,45 @@
-const mongoose = require('mongoose')
-const Book = require('../models/books')
-const dataController =(req,res)=> {
-    const {book_name,book_image,book_publication,book_author,book_comments1_author,book_comments1_content,book_comments2_author,book_comments2_content,book_comments3_author,book_comments3_content} = req.body
-}
+const mongoose = require("mongoose");
+const Book = require("../models/books");
+const dataController =async (req, res) => {
+    try {
+        const {
+            book_name,
+            book_image,
+            book_publication,
+            book_author,
+            book_comments1_author,
+            book_comments1_content,
+            book_comments2_author,
+            book_comments2_content,
+            book_comments3_author,
+            book_comments3_content,
+          } = req.body;
+        
+          const book = new Book({
+            book_name,
+            book_image,
+            book_publication,
+            book_author,
+            book_comments: {
+                book_comments1:{
+                    book_comments1_author,
+                    book_comments1_content
+                },
+                book_comments2:{
+                    book_comments2_author,
+                    book_comments2_content
+                },
+                book_comments3:{
+                    book_comments3_author,
+                    book_comments3_content
+                }
+            },
+          });
+          await book.save()
+
+          res.json({msg:'Book Saved...'})
+    } catch (error) {
+        res.json(error.message)
+    }
+ 
+};
